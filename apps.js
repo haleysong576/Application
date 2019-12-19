193d3d75c9aabba28f4fdb09b07ef1ea82233645
haleysong576 <53978880+haleysong576@users.noreply.github.com>
2019-12-19 17:15:32 -0500

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+//screens
+import MainScreen from './screens/MainScreen';
+import InfoMenuScreen from './screens/4Menus/InfoMenuScreen';
+import SettingScreen from './screens/4Menus/SettingScreen';
+import DailyTrackerScreen from './screens/4Menus/DailyTrackerScreen';
+import RecipeScreen from './screens/4Menus/RecipeScreen';
+import Chicken from './screens/Recipes/Chicken';
+
+class HomeScreen extends Component{
+  static navigationOptions = {
+    header: null
+  }
+  state = { username: "", password: "" }
+
+  checkLogin() {
+    const { username, password } = this.state
+    if(username == 'haley' && password == 'haley'){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  onPressButton() {
+    if (this.checkLogin() == true){
+      this.props.navigation.navigate('Main');
+    }
+    else {
+      console.warn("Login Failed")
+    }
+  }
+  render(){
+    const {navigate} = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Text style = {styles.title}>Amino Tracker</Text>
+        <TextInput style = {styles.label} placeholder="Username" underlineColorAndroid = {'gray'} onChangeText = {text => this.setState ({username: text })}/>
+        <TextInput style = {styles.label} placeholder="Password" underlineColorAndroid = {'gray'} secureTextEntry={true} onChangeText = {text => this.setState ({password: text })}/>
+        <View style = {styles.button}>
+          <Button 
+          onPress = {() => this.onPressButton()}
+          title = "Log in"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+
+
+
+const RootStack = createStackNavigator(
+  {
+    Home: {screen: HomeScreen},
+    Main: {screen: MainScreen},
+    InfoMenu: {screen: InfoMenuScreen},
+    Settings: {screen: SettingScreen},
+    DailyTracker: {screen: DailyTrackerScreen},    
+    Recipe: {screen: RecipeScreen},
+    Chicken: {screen: Chicken},
+  },
+  
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+
+export default class registration extends Component {
+  
+  render() {
+    return (
+      <AppContainer/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    padding: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'green',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  label: {
+    alignSelf: 'stretch',
+    fontSize: 20,
+    padding: 20,
+    color: 'black',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    color: 'black'
+  },
+  button: {
+    marginBottom: 100,
+    marginTop: 30,
+    width: 200,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  }
+});
