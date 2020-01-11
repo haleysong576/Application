810ff5a9d4a658185ad287d350d47cddc656c0eb
haleysong576 <53978880+haleysong576@users.noreply.github.com>
2020-01-11 17:22:57 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,3 +1,39 @@
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config' )
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(express.urlencoded({extended: true}));
+//app.use(express.json());
+ 
+
+//Import routes
+
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute); 
+
+
+//ROUTES  
+app.get('/', (req, res) => {
+    res.send('We are on home');
+});
+
+//connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION,  
+    { useNewUrlParser: true },
+    () => console.log('connected to DB!')
+);
+
+
+app.listen(3000);
+
+
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 import {createAppContainer} from 'react-navigation';
